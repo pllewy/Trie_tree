0a2b3c22c9684df75b34dc1ee4072af28fb3f331
s444427 <pllewyx@gmail.com>
2024-02-03 17:51:31 +0100

    ADD full ready Trie Tree

--- main.cpp
+++ main.cpp
@@ -132,8 +132,6 @@ void massLoader(Node *root, string filename, int maxWords, int currentWords = 0)
     int c;
     string ch;
 
-//    cout << "Loading..." << endl;
-
     ifstream fin(filename, fstream::in);
     if (fin.is_open()) {
         while (fin >> ch && count < maxWords) {
@@ -159,8 +157,15 @@ void massLoader(Node *root, string filename, int maxWords, int currentWords = 0)
     } else {
         cerr << "Unable to open the file for loading." << endl;
     }
-    if (count < maxWords)
+    if (count < maxWords) {
         cout << "Loaded: " << count << " words." << endl;
+        cout << "Prev file: " << filename << endl;
+//        cout << "special sign: " << char(int(filename[filename.size() - 5]) + 1) << " "  << int(filename[filename.size() - 5]) << " " << filename[filename.size() - 5] << " " << endl;
+        filename[filename.size() - 5] = char(int(filename[filename.size() - 5]) + 1);
+        cout << "New file: " << filename << endl;
+
+        massLoader(root, filename, maxWords, count);
+    }
 }
 
 Node * loader(string filename) {
@@ -183,10 +188,10 @@ Node * loader(string filename) {
 int main() {
     Node *root = new Node();
     int c;
-    int prepare_tree_level = 3;
-    int prepare_test_level = 3;
-    int test_tree_level = 3;
-    int test_test_level = 3;
+    int prepare_tree_level = 30; // 1000, 2000, 3000, 4000, 5000
+    int prepare_test_level = 4; // 10, 100, 1000, 10000
+    int test_tree_level = 30;
+    int test_test_level = 4;
     string work_mode = "test";
 
     if(work_mode == "prepare") {
@@ -196,13 +201,18 @@ int main() {
             c = 0;
             massLoader(root, "./data/txt_files/big.txt", max_words * 1000, countWords(root, c));
 
+            c = 0;
+            if (countWords(root, c) < max_words * 1000){
+                cout << "Words: " << c << endl;
+            }
+
             string path = "./data/Trie/" + to_string(max_words * 1000) + ".txt";
             saveManager(root, path);
         }
 
         Node *test_prep = new Node();
 //    PREPARE TESTS
-        for (int max_words = 1; max_words <= prepare_tree_level; max_words++) {
+        for (int max_words = 1; max_words <= prepare_test_level; max_words++) {
             c = 0;
             int words = pow(10, max_words);
             massLoader(test_prep, "./data/txt_files/rfc1.txt", words, countWords(test_prep, c));
@@ -224,35 +234,40 @@ int main() {
         string label = "";
         string batch_label = "";
         int margin=0;
+        int column_width = 20;
 
 //    TESTS
-        for (int max_words = 0; max_words <= test_tree_level; max_words++) {
+        for (int max_words = 0; max_words <= test_test_level; max_words++) {
             if (max_words == 0)
                 label = "trie";
             else
                 label = to_string(int(pow(10, max_words)));
-            margin = 20 - label.length();
+            margin = column_width - label.length();
             cout << string(margin/2, ' ') << label << string(margin-margin/2, ' ');
         }
         cout << endl;
         for (int max_words = 1; max_words <= test_tree_level; max_words++) {
-            result_count = 0;
             tree_path = "./data/Trie/" + to_string(max_words * 1000) + ".txt";
             tree = loader(tree_path);
-//        cout << "Tree: " << tree_path << endl;
-//        cout << "Test: " << test_path << endl;
+
 
             ifstream file(tree_path, ios::binary | ios::ate);
             tree_size = file.tellg();
             file.close();
 
             label = to_string(max_words * 1000) + "W[" + to_string(tree_size) + "B]";
-            margin = 20 - label.length();
+            margin = column_width - label.length();
             cout <<  label << string(margin, ' ');
 
             for (int z = 1; z <= test_test_level; z++) {
                 auto start = std::chrono::high_resolution_clock::now();
-                test_path = "./data/tests/" + to_string(int(pow(10, max_words))) + ".txt";
+                int num_of_tests = pow(10, z);
+                test_path = "./data/tests/" + to_string(num_of_tests) + ".txt";
+                result_count = 0;
+
+//                cout<<endl;
+//                cout << "Tree: " << tree_path << endl;
+//                cout << "Test: " << test_path << endl;
 
                 ifstream fin(test_path, fstream::in);
                 if (fin.is_open()) {
@@ -269,11 +284,9 @@ int main() {
                 auto stop = std::chrono::high_resolution_clock::now();
                 auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
 
-//                cout << "Function runtime: " << duration.count() << " microseconds" << endl;
-
-                result_procent = result_count/(max_words*1000.0) * 100;
+                result_procent = double (result_count)/(num_of_tests) * 100;
                 batch_label = "[" + to_string(result_procent) + "%," + to_string(duration.count()) + "mis]";
-                margin = 20 - batch_label.length();
+                margin = column_width - batch_label.length();
                 cout << string(margin/2, ' ') << batch_label << string(margin-margin/2, ' ');
             }
             cout << endl;
